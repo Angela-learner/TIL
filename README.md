0dd880a1671651cca2eeeb6ff926961ac8178bfd
Angela <ohkim0904@gmail.com>
2020-11-22 23:26:02 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TIL
+Record of what I studied 
---
+++ config.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  url: 'https://angelaohkim.netlify.app',
+  pathPrefix: '/',
+  title: 'Angelaohkim',
+  subtitle: 'Record of what I learned today',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'ALL',
+      path: '/'
+    },
+    {
+      label: 'Categories',
+      path: '/categories'
+    },
+    {
+      label: 'Tags',
+      path: '/tags'
+    },
+    // {
+    //   label: 'About me',
+    //   path: '/pages/about'
+    // }
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // }
+  ],
+  author: {
+    name: 'Angelaohkim',
+    photo: '/photo.jpg',
+    bio: 'Record of what I learned today',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
